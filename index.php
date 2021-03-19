b6b06e30cfc36c196a551250bd36b2f5c3a5b7af
Pieter Daud Satumalay <peter.crusties@gmail.com>
2021-03-19 11:01:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,19 @@
 <?php echo "Cloud Admin - PHP v1!"; ?>
 <?php echo "Hello World ITX!"; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br /> ?>
+  
+<?php $links = [];
+  foreach($_ENV as $key => $value) {
+    if(preg_match("/^(.*)_PORT_([0-9]*)_(TCP|UDP)$/", $key, $matches)) {
+      $links[] = [
+        "name"  => $matches[1],
+        "port"  => $matches[2],
+        "proto" => $matches[3],
+        "value" => $value
+      ];
+    }
+  }
+?>
 </center>
 </body>
   
