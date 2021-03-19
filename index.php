a533b78ec0b3b53ef4fed598e5e1bcbcd1b27252
Pieter Daud Satumalay <peter.crusties@gmail.com>
2021-03-19 10:51:54 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+
+<head>
+  <title>Hello World!</title>
+</head>
+  <body>
+    <?php echo "Cloud Admin - PHP v1!"; ?>
+    <?php echo "Hello World ITX!"; ?>
+    <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
+  </body>
+</html>
