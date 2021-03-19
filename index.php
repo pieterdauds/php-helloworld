f29aafbb7909d19655b6306cf28c4a0597fe90f1
Pieter Daud Satumalay <peter.crusties@gmail.com>
2021-03-19 10:55:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <html>
 
 <head>
-  <title>Hello World!</title>
+<title>Hello World!</title>
 </head>
-  <body>
-    <center>
-    <?php echo "Cloud Admin - PHP v1!"; ?>
-    <?php echo "Hello World ITX!"; ?>
-    <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
-    </center>
-  </body>
+<body>
+<center>
+<?php echo "Cloud Admin - PHP v1!"; ?>
+<?php echo "Hello World ITX!"; ?>
+<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
+</center>
+</body>
+  
 </html>
